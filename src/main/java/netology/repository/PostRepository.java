f61e54171ec992235e516e5af761e76abf8cbbbe
Yulya Lukicheva <104934302+DrowsyAlchemist@users.noreply.github.com>
2023-07-22 15:50:13 +0300

    Code refactoring and PostRepository class implementation

--- src/main/java/netology/repository/PostRepository.java
+++ src/main/java/netology/repository/PostRepository.java
@@ -1,25 +1,45 @@
 package netology.repository;
 
+import netology.exception.NotFoundException;
 import netology.model.Post;
-
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
 public class PostRepository {
+  private final ConcurrentHashMap<Long, Post> posts;
+  private final AtomicLong idCounter = new AtomicLong(0L);
+
+  public PostRepository(){
+    posts = new ConcurrentHashMap<>();
+  }
   public List<Post> all() {
-    return Collections.emptyList();
+    return new ArrayList<>(posts.values());
   }
-
   public Optional<Post> getById(long id) {
-    return Optional.empty();
+    return Optional.ofNullable(posts.get(id));
   }
-
   public Post save(Post post) {
+    if (post.getId() != 0 && !posts.containsKey(post.getId())){
+      throw new NotFoundException();
+    }else {
+      posts.put(post.getId(), post);
+    }
+
+    if (post.getId() == 0){
+      long newId = idCounter.incrementAndGet();
+      post.setId(newId);
+      posts.put(post.getId(), post);
+    }
     return post;
   }
-
   public void removeById(long id) {
+    if (posts.containsKey(id)){
+      posts.remove(id);
+    }else {
+      throw new NotFoundException("Вы ввели не верный id");
+    }
   }
 }
--- src/main/java/netology/servlet/MainServlet.java
+++ src/main/java/netology/servlet/MainServlet.java
@@ -7,43 +7,47 @@ import netology.service.PostService;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
-  private PostController controller;
+  private PostController postController;
+  public static final String API_POSTS = "/api/posts";
+  public static final String API_POSTS_ID = "/api/posts/\\d+";
+  public static final String STR = "/";
+  public static final String METHOD_GET = "GET";
+  public static final String METHOD_POST = "POST";
+
+  public static final String METHOD_DELETE = "DELETE";
 
   @Override
   public void init() {
     final var repository = new PostRepository();
     final var service = new PostService(repository);
-    controller = new PostController(service);
+    postController = new PostController(service);
   }
 
   @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    // если деплоились в root context, то достаточно этого
+  protected void service(HttpServletRequest req, HttpServletResponse resp) {
+
     try {
       final var path = req.getRequestURI();
       final var method = req.getMethod();
-      // primitive routing
-      if (method.equals("GET") && path.equals("/api/posts")) {
-        controller.all(resp);
+
+      if (method.equals(METHOD_GET) && path.equals(API_POSTS)) {
+        postController.all(resp);
         return;
       }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.getById(id, resp);
+      if (method.equals(METHOD_GET) && path.matches(API_POSTS_ID)) {
+        final var id = Long.parseLong(path.substring(path.lastIndexOf(STR)+1));
+        postController.getById(id, resp);
         return;
       }
-      if (method.equals("POST") && path.equals("/api/posts")) {
-        controller.save(req.getReader(), resp);
+      if (method.equals(METHOD_POST) && path.equals(API_POSTS)) {
+        postController.save(req.getReader(), resp);
         return;
       }
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.removeById(id, resp);
+      if (method.equals(METHOD_DELETE) && path.matches(API_POSTS_ID)) {
+        final var id = Long.parseLong(path.substring(path.lastIndexOf(STR)+1));
+        postController.removeById(id, resp);
         return;
       }
       resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
